08b5840096458787321777e5f6a309be6b9845b7
Eric Lippmann <eric.lippmann@netways.de>
2014-08-26 16:35:30 +0200

    Move Commandpipe/* classes from Icinga Web 2's library to the monitoring module
    refs #6593

---
+++ library/Monitoring/Command/Exception/InvalidCommandException.php
@@ -0,0 +1,12 @@
+<?php
+// {{{ICINGA_LICENSE_HEADER}}}
+// {{{ICINGA_LICENSE_HEADER}}}
+
+namespace Icinga\Protocol\Commandpipe\Exception;
+
+/**
+ * Exception class for unknown/invalid external commands
+ */
+class InvalidCommandException extends \Exception
+{
+}
